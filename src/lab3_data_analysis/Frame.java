5b981df9b853055b7816c2f02ff91985fa1de182
Prisyazhnuy <prisyazhnuy@gmail.com>
2014-01-28 18:00:29 +0200

    Finish form mediana

--- src/lab3_data_analysis/Frame.java
+++ src/lab3_data_analysis/Frame.java
@@ -33,8 +33,13 @@ public class Frame extends javax.swing.JFrame {
         jTblSecond = new javax.swing.JTable();
         jScrollPane4 = new javax.swing.JScrollPane();
         jTblThird = new javax.swing.JTable();
-        jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        jBtnUniformity = new javax.swing.JButton();
+        jScrollPane7 = new javax.swing.JScrollPane();
+        jTblStatisticMed = new javax.swing.JTable();
+        jSldrMed = new javax.swing.JSlider();
+        jTFSliderMed = new javax.swing.JTextField();
+        jScrollPane8 = new javax.swing.JScrollPane();
+        jTAMed = new javax.swing.JTextArea();
         jPanel2 = new javax.swing.JPanel();
         jBtnOpenFileFridman = new javax.swing.JButton();
         jScrollPane5 = new javax.swing.JScrollPane();
@@ -88,14 +93,38 @@ public class Frame extends javax.swing.JFrame {
         jTblThird.setModel(modelThird);
         jScrollPane4.setViewportView(jTblThird);
 
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jBtnUniformity.setText("Проверить");
+        jBtnUniformity.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jBtnUniformityActionPerformed(evt);
             }
         });
 
-        jTextField1.setText("jTextField1");
+        modelStatisticMed = new DefaultTableModel(5, 1);
+        modelStatisticMed.setValueAt("Среднее", 0, 0);
+        modelStatisticMed.setValueAt("Среднеквадратическое", 1, 0);
+        modelStatisticMed.setValueAt("Коефициент ассиметрии", 2, 0);
+        modelStatisticMed.setValueAt("Коефициент эксцессу", 3, 0);
+        modelStatisticMed.setValueAt("Коефициент вариации", 4, 0);
+        jTblStatisticMed.setModel(modelStatisticMed);
+        jScrollPane7.setViewportView(jTblStatisticMed);
+
+        jSldrMed.setMajorTickSpacing(10);
+        jSldrMed.setMinorTickSpacing(5);
+        jSldrMed.setPaintLabels(true);
+        jSldrMed.setPaintTicks(true);
+        jSldrMed.setValue(5);
+        jSldrMed.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jSldrMedMouseDragged(evt);
+            }
+        });
+
+        jTFSliderMed.setText("0.05");
+
+        jTAMed.setColumns(20);
+        jTAMed.setRows(5);
+        jScrollPane8.setViewportView(jTAMed);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -103,24 +132,26 @@ public class Frame extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane4, javax.swing.GroupLayout.DEFAULT_SIZE, 523, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jBtnOpenFile1, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
-                            .addComponent(jBtnOpenFile2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jBtnOpenFile2, javax.swing.GroupLayout.DEFAULT_SIZE, 255, Short.MAX_VALUE)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
+                    .addComponent(jScrollPane7)
+                    .addComponent(jScrollPane4)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                        .addComponent(jSldrMed, javax.swing.GroupLayout.PREFERRED_SIZE, 188, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jTFSliderMed, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane8, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jBtnUniformity)))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -131,15 +162,22 @@ public class Frame extends javax.swing.JFrame {
                     .addComponent(jBtnOpenFile2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(57, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(11, 11, 11)
+                        .addComponent(jBtnUniformity, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jTFSliderMed, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jSldrMed, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane8, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Медианный критерий", jPanel1);
@@ -180,14 +218,14 @@ public class Frame extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 523, Short.MAX_VALUE)
+                    .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE)
                     .addComponent(jScrollPane6)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jBtnOpenFileFridman)
                             .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addComponent(jButton2)
-                                .addGap(18, 18, 18)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
@@ -199,13 +237,13 @@ public class Frame extends javax.swing.JFrame {
                 .addComponent(jBtnOpenFileFridman)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 251, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(37, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton2))
+                .addContainerGap(64, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Критерий Фридмана", jPanel2);
@@ -223,54 +261,6 @@ public class Frame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jBtnOpenFile1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnOpenFile1ActionPerformed
-        first = new OrderedSeries();
-        jFileChooser1.showDialog(null, "Open first file");
-        try {
-            first.loadFromFile(jFileChooser1.getSelectedFile().getPath());
-            if(third==null){
-                third = new OrderedSeries();
-                third.loadFromFile(jFileChooser1.getSelectedFile().getPath());
-            }
-            else{
-                third.loadFromFile(jFileChooser1.getSelectedFile().getPath());
-            }
-            first.showOrderedSeries(modelFirst);
-            modelThird=first.getMedians(modelThird, 1);
-        } catch (FileNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "File not found");
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, "File has wrong information");
-        }
-        
-    }//GEN-LAST:event_jBtnOpenFile1ActionPerformed
-
-    private void jBtnOpenFile2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnOpenFile2ActionPerformed
-        second = new OrderedSeries();
-        jFileChooser2.showDialog(null, "Open second file");
-        try {
-            second.loadFromFile(jFileChooser2.getSelectedFile().getPath());
-            if(third==null){
-                third = new OrderedSeries();
-                third.loadFromFile(jFileChooser2.getSelectedFile().getPath());
-            }
-            else{
-                third.loadFromFile(jFileChooser2.getSelectedFile().getPath());
-            }
-            second.showOrderedSeries(modelSecond);
-            modelThird=second.getMedians(modelThird, 2);
-        } catch (FileNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "File not found");
-        } catch (IOException ex) {
-            JOptionPane.showMessageDialog(null, "File has wrong information");
-        }
-        
-    }//GEN-LAST:event_jBtnOpenFile2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jTextField1.setText(""+first.odnorodnost2(modelThird));
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jBtnOpenFileFridmanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnOpenFileFridmanActionPerformed
         series = new OrderedSeries();
@@ -291,6 +281,69 @@ public class Frame extends javax.swing.JFrame {
         ArrayList<ArrayList<Double>> ranks = Rank.countRanks(arrayOfX);
         jTextField2.setText(""+Rank.isUniformityFridman(ranks));
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jBtnUniformityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnUniformityActionPerformed
+        double U = first.getUniformityMed(modelThird);
+        double Kv = Distributions.NormInv(1-Double.parseDouble(jTFSliderMed.getText())/2.0, 0, 1);
+        String str = "U="+U+"\n";
+        str+="Квантиль="+Kv+"\n";
+        if(Math.abs(U)<=Kv){
+            str+="Принимается";
+        }else{
+            str+="Не принимается";
+        }
+        jTAMed.setText(str);
+    }//GEN-LAST:event_jBtnUniformityActionPerformed
+
+    private void jBtnOpenFile2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnOpenFile2ActionPerformed
+        second = new OrderedSeries();
+        jFileChooser2.showDialog(null, "Open second file");
+        try {
+            second.loadFromFile(jFileChooser2.getSelectedFile().getPath());
+            if(third==null){
+                third = new OrderedSeries();
+                third.loadFromFile(jFileChooser2.getSelectedFile().getPath());
+            }
+            else{
+                third.loadFromFile(jFileChooser2.getSelectedFile().getPath());
+            }
+            second.showOrderedSeries(modelSecond);
+            second.showStatistics(modelStatisticMed);
+            modelThird=second.getMedians(modelThird, 2);
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "File not found");
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "File has wrong information");
+        }
+
+    }//GEN-LAST:event_jBtnOpenFile2ActionPerformed
+
+    private void jBtnOpenFile1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnOpenFile1ActionPerformed
+        first = new OrderedSeries();
+        jFileChooser1.showDialog(null, "Open first file");
+        try {
+            first.loadFromFile(jFileChooser1.getSelectedFile().getPath());
+            if(third==null){
+                third = new OrderedSeries();
+                third.loadFromFile(jFileChooser1.getSelectedFile().getPath());
+            }
+            else{
+                third.loadFromFile(jFileChooser1.getSelectedFile().getPath());
+            }
+            first.showOrderedSeries(modelFirst);
+            first.showStatistics(modelStatisticMed);
+            modelThird=first.getMedians(modelThird, 1);
+        } catch (FileNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "File not found");
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "File has wrong information");
+        }
+
+    }//GEN-LAST:event_jBtnOpenFile1ActionPerformed
+
+    private void jSldrMedMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jSldrMedMouseDragged
+        jTFSliderMed.setText(""+jSldrMed.getValue()/(double)100);
+    }//GEN-LAST:event_jSldrMedMouseDragged
 
     public static void start() {
         try {
@@ -321,7 +374,7 @@ public class Frame extends javax.swing.JFrame {
     private javax.swing.JButton jBtnOpenFile1;
     private javax.swing.JButton jBtnOpenFile2;
     private javax.swing.JButton jBtnOpenFileFridman;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jBtnUniformity;
     private javax.swing.JButton jButton2;
     private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JFileChooser jFileChooser2;
@@ -334,14 +387,19 @@ public class Frame extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JScrollPane jScrollPane5;
     private javax.swing.JScrollPane jScrollPane6;
+    private javax.swing.JScrollPane jScrollPane7;
+    private javax.swing.JScrollPane jScrollPane8;
+    private javax.swing.JSlider jSldrMed;
+    private javax.swing.JTextArea jTAMed;
+    private javax.swing.JTextField jTFSliderMed;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTblFirst;
     private javax.swing.JTable jTblFridman;
     private javax.swing.JTable jTblSecond;
     private javax.swing.JTable jTblStatistic;
+    private javax.swing.JTable jTblStatisticMed;
     private javax.swing.JTable jTblThird;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 private OrderedSeries first = null;
@@ -353,5 +411,6 @@ private DefaultTableModel modelSecond;
 private DefaultTableModel modelThird;
 private DefaultTableModel modelFridman;
 private DefaultTableModel modelStatistic;
+private DefaultTableModel modelStatisticMed;
 private ArrayList<Vector<Double>> arrayOfX = new ArrayList<>();
 }
